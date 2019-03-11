4662fc64601c86e31d9bd07022f4cca53748b1bb
Missi3223 <44407731+Missi3223@users.noreply.github.com>
2019-03-11 13:26:40 -0600

    merge

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
+  var User = sequelize.define("User", {
     text: DataTypes.STRING,
     description: DataTypes.TEXT
   });
---
+++ schema.sql
@@ -0,0 +1,8 @@
+-- DROP DATABASE IF EXISTS user_name;
+CREATE DATABASE Adult_Pokemon_db;
+USE Adult_Pokemon_db
+
+ 
+
+
+
