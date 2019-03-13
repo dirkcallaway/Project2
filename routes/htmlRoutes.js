826be154dcd8574f77433831ecbea8d6c65838cb
Missi3223 <44407731+Missi3223@users.noreply.github.com>
2019-03-12 21:07:06 -0600

    seeder

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,9 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Quest.findAll({ where: { location: req.body.location } }).then(function(
+      dbExamples
+    ) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -12,7 +14,7 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
+  app.get("/user/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
