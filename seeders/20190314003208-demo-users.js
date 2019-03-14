00bb6fab278f6e7f50b892414a81d1a1fccb4e11
Missi3223 <44407731+Missi3223@users.noreply.github.com>
2019-03-14 08:55:09 -0600

    fixed error and added map for Broomfield

--- seeders/20190314003208-demo-users.js
+++ seeders/20190314003208-demo-users.js
@@ -1,33 +1,29 @@
-'use strict';
+// 'use strict';
 
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users',[{
-      UserName: 'John',
-      questComplete: false
+// module.exports = {
+//   up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Users', [{
+//     UserName: 'John',
+//     questComplete: false
+//   }], {}),
 
-    }],{});
-  },
+//     /*
+//       Add altering commands here.
+//       Return a promise to correctly handle asynchronicity.
 
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+//       Example:
+//       return queryInterface.bulkInsert('People', [{
+//         name: 'John Doe',
+//         isBetaMember: false
+//       }], {});
+//     */
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+//   down: (queryInterface, Sequelize) => {
+//     /*
+//       Add reverting commands here.
+//       Return a promise to correctly handle asynchronicity.
 
+//       Example:*/
+//       return queryInterface.bulkDelete('Users', null, {});
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:*/
-      return queryInterface.bulkDelete('Users', null, {});
-
-  }
-};
+//   }
+// };
