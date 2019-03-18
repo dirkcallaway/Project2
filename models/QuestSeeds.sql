f4e5e51b03acbc1fc06da4573640ff62b18eeabd
Dirk Callaway <35826733+dirkcallaway@users.noreply.github.com>
2019-03-17 21:37:23 -0600

    Added rewards model and rewardsSeeds file

--- models/QuestSeeds.sql
+++ models/QuestSeeds.sql
@@ -1,26 +1,26 @@
 USE project2_db;
 
-INSERT INTO quests (questName, questDescribe, Location)
-VALUES ("Snooze an A.M. Eatery", "Start your day off with an egg dish", "Fort Collins");
-INSERT INTO quests (questName, questDescribe,Location)
-VALUES ("CopperMuse Distillery", "Take a tour" , "Fort Collins");
-INSERT INTO quests (questName, questDescribe,Location)
-VALUES ("Beau Jo's" , "Buy any large pizza", "Fort Collins");
-INSERT INTO quests (questName, questDescribe,Location)
-VALUES ("Curiosities", "Spend $10", "Fort Collins");
-INSERT INTO quests (questName, questDescribe,Location)
-VALUES ("Nature's Own", "Spend $20", "Fort Collins");
-INSERT INTO quests (questName, questDescribe,Location)
-VALUES ("Town Pump" , "Grab some cherries", "Fort Collins");
-INSERT INTO quests (questName, questDescribe,Location)
-VALUES ("Original Pizza", "Stop in a grab a Large pizza" , "Broomfield");
-INSERT INTO quests (questName, questDescribe,Location)
-VALUES ("At Home", "Spend $25" , "Broomfield");
-INSERT INTO quests (questName, questDescribe,Location)
-VALUES ("Chipper's Lane" , "Buy Two Pitchers of beer", "Broomfield");
-INSERT INTO quests (questName, questDescribe,Location)
-VALUES ("Night Owl Lounge", "Buy a pitcher of beer, and play a round of pool", "Broomfield");
-INSERT INTO quests (questName, questDescribe,Location)
-VALUES ("Infinitus Pizza PIE", "Buy a medium pizza", "Broomfield");
-INSERT INTO quests (questName, questDescribe,Location)
-VALUES ("Shell" , "Purchase 1 tank of gas", "Broomfield");
+INSERT INTO quests (questName, questDescribe, Location, completed)
+VALUES ("Snooze an A.M. Eatery", "Start your day off with an egg dish", "Fort Collins", false);
+INSERT INTO quests (questName, questDescribe, Location, completed)
+VALUES ("CopperMuse Distillery", "Take a tour" , "Fort Collins", false),
+
+("Beau Jo's" , "Buy any large pizza", "Fort Collins" , false),
+
+("Curiosities", "Spend $10", "Fort Collins", false),
+
+("Nature's Own", "Spend $20", "Fort Collins", false),
+
+("Town Pump" , "Grab some cherries", "Fort Collins" ,false),
+
+("Original Pizza", "Stop in a grab a Large pizza" , "Broomfield", false),
+
+("At Home", "Spend $25" , "Broomfield", false),
+
+("Chipper's Lane" , "Buy Two Pitchers of beer", "Broomfield", false),
+
+("Night Owl Lounge", "Buy a pitcher of beer, and play a round of pool", "Broomfield", false),
+
+("Infinitus Pizza PIE", "Buy a medium pizza", "Broomfield", false),
+
+("Shell" , "Purchase 1 tank of gas", "Broomfield", false);
